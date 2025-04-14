88d4093dfc4aec7cd6924425c3c0b5b8ae1bb488
sjha178 <55937839+sjha178@users.noreply.github.com>
2025-04-14 19:08:38 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,3 +1,6 @@
-def clone(String url, String branch){
-  git url: url, branch: branch
+def call(String repoUrl, String branchName) {
+    checkout([$class: 'GitSCM',
+        branches: [[name: "*/${branchName}"]],
+        userRemoteConfigs: [[url: repoUrl]]
+    ])
 }
