fdf3aff60e890172a0d5148a8dc34ef8d24ac79e
sjha178 <55937839+sjha178@users.noreply.github.com>
2025-04-14 19:31:28 +0530

    Create push.groovy

---
+++ vars/push.groovy
@@ -0,0 +1,14 @@
+def call(String credId,String img, String tag){
+
+  withCredentials([usernamePassword(
+                    credentialsId:"${credId}",
+                    passwordVariable:"dockerHubPass",
+                    usernameVariable:"dockerHubUser")])
+                {
+                echo " pushing the image create to docker hun"
+                sh 'docker tag "${img}":"${tag}" shan365/"${img}":"${tag}"'
+                echo "docker login"
+                sh "docker login -u  ${env.dockerHubUser} -p ${env.dockerHubPass}"
+                sh 'docker push shan365/"${img}":"${tag}" '
+                }
+}
