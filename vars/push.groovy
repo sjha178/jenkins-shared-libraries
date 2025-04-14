18fbbb58b23273432e5e1c775ef8797de2a110e5
sjha178 <55937839+sjha178@users.noreply.github.com>
2025-04-14 19:44:49 +0530

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,34 +1,34 @@
-def call(String credId,String img, String tag){
+// def call(String credId,String img, String tag){
 
-  withCredentials([usernamePassword(
-                    credentialsId: credId,
-                    passwordVariable:"dockerHubPass",
-                    usernameVariable:"dockerHubUser")])
-                {
-                echo " pushing the image create to docker hun"
-                sh "docker tag ${img}:{tag} shan365/${img}:${tag}"
-                echo "docker login"
-                sh "docker login -u  ${env.dockerHubUser} -p ${env.dockerHubPass}"
-                sh "docker push shan365/${img}:${tag}"
-                }
-}
-
-// def call(Map config = [:]) {
-//     def imageName = config.imageName ?: error("Image name is required")
-//     def imageTag = config.imageTag ?: 'latest'
-//     def credentials = config.credentials ?: 'docker-hub-credentials'
-    
-//     echo "Pushing Docker image: ${imageName}:${imageTag}"
-    
-//     withCredentials([usernamePassword(
-//         credentialsId: credentials,
-//         usernameVariable: 'DOCKER_USERNAME',
-//         passwordVariable: 'DOCKER_PASSWORD'
-//     )]) {
-//         sh """
-//             echo "\$DOCKER_PASSWORD" | docker login -u "\$DOCKER_USERNAME" --password-stdin
-//             docker push ${imageName}:${imageTag}
-//             docker push ${imageName}:latest
-//         """
-//     }
+//   withCredentials([usernamePassword(
+//                     credentialsId: credId,
+//                     passwordVariable:"dockerHubPass",
+//                     usernameVariable:"dockerHubUser")])
+//                 {
+//                 echo " pushing the image create to docker hun"
+//                 sh "docker tag ${img}:{tag} shan365/${img}:${tag}"
+//                 echo "docker login"
+//                 sh "docker login -u  ${env.dockerHubUser} -p ${env.dockerHubPass}"
+//                 sh "docker push shan365/${img}:${tag}"
+//                 }
 // }
+
+def call(Map config = [:]) {
+    def imageName = config.imageName ?: error("Image name is required")
+    def imageTag = config.imageTag ?: 'latest'
+    def credentials = config.credentials ?: 'docker-hub-credentials'
+    
+    echo "Pushing Docker image: ${imageName}:${imageTag}"
+    
+    withCredentials([usernamePassword(
+        credentialsId: credentials,
+        usernameVariable: 'DOCKER_USERNAME',
+        passwordVariable: 'DOCKER_PASSWORD'
+    )]) {
+        sh """
+            echo "\$DOCKER_PASSWORD" | docker login -u "\$DOCKER_USERNAME" --password-stdin
+            docker push ${imageName}:${imageTag}
+            docker push ${imageName}:latest
+        """
+    }
+}
