dae752f7b2c1f64deb87ea97a1ce86f1c6605729
sjha178 <55937839+sjha178@users.noreply.github.com>
2025-04-14 19:21:19 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,7 +1,7 @@
 // def call(String url, String branch){
 //   git url: "${url}", branch: "${branch}"
 // }
-def clone(String url, String branch){
+def call(String url, String branch){
   git url: "${url}", branch: "${branch}"
 }
 
