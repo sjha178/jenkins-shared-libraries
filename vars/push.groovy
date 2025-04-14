2a7c3b6d653fb5ae73d0fc629d1adddb8877ea85
sjha178 <55937839+sjha178@users.noreply.github.com>
2025-04-14 19:42:28 +0530

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -13,20 +13,22 @@
 //                 }
 // }
 
-def call(String credId, String img, String tag) {
+def call(Map config = [:]) {
+    def imageName = config.imageName ?: error("Image name is required")
+    def imageTag = config.imageTag ?: 'latest'
+    def credentials = config.credentials ?: 'docker-hub-credentials'
+    
+    echo "Pushing Docker image: ${imageName}:${imageTag}"
+    
     withCredentials([usernamePassword(
-        credentialsId: credId,
-        passwordVariable: 'dockerHubPass',
-        usernameVariable: 'dockerHubUser'
+        credentialsId: credentials,
+        usernameVariable: 'DOCKER_USERNAME',
+        passwordVariable: 'DOCKER_PASSWORD'
     )]) {
-        echo "Pushing the image to Docker Hub"
-
-        // Use double quotes for interpolation
-        sh "docker tag ${img}:${tag} shan365/${img}:${tag}"
-        
-        echo "Logging into Docker Hub"
-        sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
-        
-        sh "docker push shan365/${img}:${tag}"
+        sh """
+            echo "\$DOCKER_PASSWORD" | docker login -u "\$DOCKER_USERNAME" --password-stdin
+            docker push ${imageName}:${imageTag}
+            docker push ${imageName}:latest
+        """
     }
 }
